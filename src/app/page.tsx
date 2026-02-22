019be749713269aa9dbd60de4e9fef48967eaf2a
Pascual AB <pascualab@MacBook-Air-van-Pascual.local>
2026-02-22 21:31:16 +0100

    Move Built for Creators section above Features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <UseCases />
         <Features />
         <Examples />
         <HowItWorks />
-        <UseCases />
         <Cta />
       </main>
       <Footer />
